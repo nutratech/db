43c75e0a809a05e1619d871ac74f979bff79d5a1
gamesguru <mathmuncher11@gmail.com>
2020-06-16 12:08:13 -0400

    integrate python import

--- sql/config-db.sh
+++ sql/config-db.sh
@@ -10,5 +10,6 @@ psql -c "\encoding UTF8" postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/
 psql -c "UPDATE pg_database SET encoding=pg_char_to_encoding('UTF8') WHERE datname='$PSQL_DB_NAME';" postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/postgres
 
 # Create schema, set search_path
+psql -c "DROP SCHEMA IF EXISTS $PSQL_SCHEMA_NAME CASCADE;" postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME
 psql -c "CREATE SCHEMA $PSQL_SCHEMA_NAME;" postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME
 psql -c "ALTER ROLE $PSQL_USER SET search_path TO $PSQL_SCHEMA_NAME;" postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME
--- sql/pg.py
+++ sql/pg.py
@@ -1,10 +1,15 @@
+#!/usr/bin/env python3
 import getpass
 import os
+import sys
+from io import StringIO
 
 import psycopg2
 import psycopg2.extras
-
 from dotenv import load_dotenv
+
+# cd to script's directory
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Read in .env file if it exists locally, else look to env vars
 load_dotenv(verbose=True)
@@ -35,6 +40,72 @@ print(
     f"[Connected to Postgre DB]    postgresql://{PSQL_USER}:{PSQL_PASSWORD}@{PSQL_HOST}:5432/{PSQL_DATABASE}",
 )
 print(f"[psql] USE SCHEMA {PSQL_SCHEMA};")
+
+
+def import_():
+
+    ptables = ["users", "products", "variants", "orders", "threads"]
+
+    for f in os.listdir("../data/csv"):
+        f = os.path.splitext(f)[0]
+        if f in ptables:
+            print(f)
+            csv2sql(f)
+    print("\nother tables\n")
+    for f in os.listdir("../data/csv"):
+        f = os.path.splitext(f)[0]
+        if f not in ptables:
+            print(f)
+
+
+def csv2sql(tablename=None):
+
+    cur = con.cursor()
+
+    csv_file = f"../data/csv/{tablename}.csv"
+    df = open(csv_file)
+
+    buf = StringIO(df)
+    # df.to_csv(buf, header=True, index=False)
+    # buf.pos = 0
+
+    cur.copy_from(buf, tablename, sep=",")
+    con.commit()
+    cur.close()
+
+    query = cur.mogrify(
+        f"\\copy {tablename} FROM ../data/csv/{tablename}.csv WITH CSV HEADER"
+    )
+    print(query)
+    cur.execute(query)
+
+
+def rebuild_():
+
+    cur = con.cursor()
+
+    query = "DROP SCHEMA IF EXISTS nt CASCADE"
+    print(query)
+    cur.execute(query)
+
+    print(cur.statusmessage)
+    con.commit()
+    cur.close()
+
+
+def export_():
+    pass
+
+
+if __name__ == "__main__":
+
+    arg1 = sys.argv[1]
+    if arg1 == "i" or arg1 == "import":
+        import_()
+    elif arg1 == "r" or arg1 == "rebuild":
+        rebuild_()
+    elif arg1 == "e" or arg1 == "export":
+        export_()
 
 
 def psql(query, params=None):
