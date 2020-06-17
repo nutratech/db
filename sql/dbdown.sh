56e19c92256bbbe83a421e479befd70f2b9b4a25
gamesguru <mathmuncher11@gmail.com>
2020-06-17 08:14:31 -0400

    add cd cwd to dbup/dbdown

--- sql/dbdown.sh
+++ sql/dbdown.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
 source .env
 
 pg_ctl -D $PSQL_DB_DIR stop
--- sql/dbup.sh
+++ sql/dbup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
 source .env
 
 sudo chown -R $LOGNAME:$LOGNAME /var/run/postgresql
