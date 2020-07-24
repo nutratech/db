7f912222adb15badcb06b9a61bab7691ddc19ef4
gamesguru <mathmuncher11@gmail.com>
2020-07-24 05:26:44 -0400

    set jit=OFF in postgres v12

--- sql/sql.sh
+++ sql/sql.sh
@@ -4,4 +4,9 @@ cd "$(dirname "$0")"
 source .env
 
 # Connect to DB
-psql postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME?options=--search_path%3d$PSQL_SCHEMA_NAME
+if [ $PSQL_HOST == 'localhost' ]
+then
+    psql postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME?options="--search_path%3d$PSQL_SCHEMA_NAME --jit%3doff"
+else
+    psql postgresql://$PSQL_USER:$PSQL_PASSWORD@$PSQL_HOST:5432/$PSQL_DB_NAME?options=--search_path%3d$PSQL_SCHEMA_NAME
+fi
