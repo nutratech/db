69a11259a64a8c835a2be9f5dd3078590641022e
gamesguru <mathmuncher11@gmail.com>
2020-09-18 07:36:47 -0400

    flip db up/down sudo cmd convenience

--- sql/dbdown.sh
+++ sql/dbdown.sh
@@ -3,4 +3,5 @@
 cd "$(dirname "$0")"
 source .env
 
+sudo chown -R $LOGNAME:$LOGNAME /var/run/postgresql
 pg_ctl -D $PSQL_DB_DIR stop
--- sql/dbup.sh
+++ sql/dbup.sh
@@ -3,5 +3,4 @@
 cd "$(dirname "$0")"
 source .env
 
-sudo chown -R $LOGNAME:$LOGNAME /var/run/postgresql
 pg_ctl -D $PSQL_DB_DIR -l $PSQL_DB_DIR/pg.log start
