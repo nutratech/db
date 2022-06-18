0ef9a2987ba6f63a60ac0cfcf4424eb3251dbb58
Shane Jaroch <nutratracker@protonmail.com>
2022-06-18 14:12:55 -0400

    update autodoc

--- docs/pg-autodoc.sh
+++ docs/pg-autodoc.sh
@@ -1,27 +1,38 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 
 cd "$(dirname "$0")"
 
 source ../sql/.env
 
-DB=nutra
+DB=nt
 
 rm -rf $DB
 mkdir -p $DB
 cd $DB
 
-# Generate docs, convert DOT --> EPS
+# Generate docs
 if [ $PSQL_USER ]
 then
-    postgresql_autodoc -d $PSQL_DB_NAME -h $PSQL_HOST -u $PSQL_USER --password=$PSQL_PASSWORD -f $DB -t dot
+    postgresql_autodoc \
+        -d $PSQL_DB_NAME \
+        -h $PSQL_HOST \
+        -u $PSQL_USER \
+        --password=$PSQL_PASSWORD \
+        -f $DB \
+        -t dot
 else
-    postgresql_autodoc -d $PSQL_DB_NAME -f $DB -t dot
+    postgresql_autodoc \
+        -d $PSQL_DB_NAME \
+        -f $DB \
+        -t dot
 fi
 
-# convert --> SVG
+# convert DOT --> EPS
+# convert EPS --> SVG
 dot -Tps $DB.dot -o $DB.eps
 epstopdf $DB.eps
 pdf2svg $DB.pdf $DB.svg
 
 # Move up
 mv $DB.svg ..
+
