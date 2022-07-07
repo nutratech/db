b840163adf948f9b5b20f945e0f3ec3fd22f487d
Shane Jaroch <chown_tee@proton.me>
2022-07-07 14:05:19 -0400

    use pg_format, it's more git friendly
        less diff in whole table when only modifying one column

--- sql/tables.sql
+++ sql/tables.sql
@@ -25,34 +25,31 @@ SET client_min_messages TO WARNING;
 ---------------------------
 -- Versioning table
 ---------------------------
-CREATE TABLE "version"
-(
-  id        serial PRIMARY KEY,
+CREATE TABLE "version" (
+  id serial PRIMARY KEY,
   "version" text NOT NULL,
-  created   date DEFAULT CURRENT_DATE,
-  notes     text
+  created date DEFAULT CURRENT_DATE,
+  notes text
 );
 
 ---------------------------
 -- Location info
 ---------------------------
-CREATE TABLE country
-(
-  id             int PRIMARY KEY,
-  code           text    NOT NULL,
-  name           text    NOT NULL,
-  has_zip        boolean NOT NULL,
+CREATE TABLE country (
+  id int PRIMARY KEY,
+  code text NOT NULL,
+  name text NOT NULL,
+  has_zip boolean NOT NULL,
   requires_state boolean NOT NULL,
   UNIQUE (code),
   UNIQUE (name)
 );
 
-CREATE TABLE state
-(
-  id         int PRIMARY KEY,
-  country_id int  NOT NULL,
-  code       text NOT NULL,
-  name       text NOT NULL,
+CREATE TABLE state (
+  id int PRIMARY KEY,
+  country_id int NOT NULL,
+  code text NOT NULL,
+  name text NOT NULL,
   UNIQUE (country_id, name),
   -- UNIQUE(country_id, code),
   FOREIGN KEY (country_id) REFERENCES country (id)
@@ -62,41 +59,38 @@ CREATE TABLE state
 --++++++++++++++++++++++++++++
 -- Main users tables
 --++++++++++++++++++++++++++++
-CREATE TABLE "user"
-(
-  id         serial PRIMARY KEY,
-  username   text,
-  passwd     text,
-  created    int DEFAULT extract(epoch FROM NOW()),
+CREATE TABLE "user" (
+  id serial PRIMARY KEY,
+  username text,
+  passwd text,
+  created int DEFAULT extract(epoch FROM NOW()),
   country_id int,
-  state_id   int,
+  state_id int,
   UNIQUE (username),
   FOREIGN KEY (country_id) REFERENCES country (id) ON UPDATE CASCADE ON DELETE CASCADE,
   FOREIGN KEY (state_id) REFERENCES state (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
-CREATE TABLE email
-(
-  id        serial PRIMARY KEY,
-  user_id   int     NOT NULL,
-  email     text    NOT NULL,
+CREATE TABLE email (
+  id serial PRIMARY KEY,
+  user_id int NOT NULL,
+  email text NOT NULL,
   -- TODO: limit to 5 emails, purge old ones
-  main      boolean NOT NULL,
+  main boolean NOT NULL,
   activated boolean DEFAULT FALSE,
-  created   int     DEFAULT extract(epoch FROM NOW()),
+  created int DEFAULT extract(epoch FROM NOW()),
   UNIQUE (user_id, main),
   UNIQUE (email),
   FOREIGN KEY (user_id) REFERENCES "user" (id) ON UPDATE CASCADE
 );
 
 -- TODO: do we want to manage this? How. (e.g. analytics vs. customer_activity)
-CREATE TABLE device
-(
-  id          bigserial PRIMARY KEY,
-  user_id     int  NOT NULL,
-  token       text NOT NULL,
+CREATE TABLE device (
+  id bigserial PRIMARY KEY,
+  user_id int NOT NULL,
+  token text NOT NULL,
   last_active int DEFAULT extract(epoch FROM NOW()),
-  device_id   text NOT NULL, -- e.g. linux nutra@homecpu python-requests/2.24.0
+  device_id text NOT NULL, -- e.g. linux nutra@homecpu python-requests/2.24.0
   FOREIGN KEY (user_id) REFERENCES "user" (id) ON UPDATE CASCADE
 );
 
@@ -107,13 +101,12 @@ CREATE TABLE device
 --   -- email_token_pw_reset
 --   name text NOT NULL
 -- );
-CREATE TABLE token
-(
+CREATE TABLE token (
   -- TODO: device fingerprinting, token revocation, client-side hashing?
-  id      bigserial PRIMARY KEY,
-  user_id int  NOT NULL,
-  token   text NOT NULL,
-  type    text NOT NULL,
+  id bigserial PRIMARY KEY,
+  user_id int NOT NULL,
+  token text NOT NULL,
+  type text NOT NULL,
   created int DEFAULT extract(epoch FROM NOW()),
   expires int,
   UNIQUE (token),
@@ -127,70 +120,64 @@ CREATE TABLE token
 --++++++++++++++++++++++++++++
 -- Food recipes & custom RDAs
 --++++++++++++++++++++++++++++
-CREATE TABLE bmr_eq
-(
-  id   serial PRIMARY KEY,
+CREATE TABLE bmr_eq (
+  id serial PRIMARY KEY,
   name text NOT NULL
 );
 
-CREATE TABLE bf_eq
-(
-  id   serial PRIMARY KEY,
+CREATE TABLE bf_eq (
+  id serial PRIMARY KEY,
   name text NOT NULL
 );
 
-CREATE TABLE profile
-(
-  id             serial PRIMARY KEY,
-  guid           text NOT NULL UNIQUE,
-  user_id        int  NOT NULL,
-  created        int DEFAULT extract(epoch FROM NOW()),
+CREATE TABLE profile (
+  id serial PRIMARY KEY,
+  guid text NOT NULL UNIQUE,
+  user_id int NOT NULL,
+  created int DEFAULT extract(epoch FROM NOW()),
   -- TODO: trigger for last updated
-  updated        int DEFAULT extract(epoch FROM NOW()),
-  eula           int DEFAULT 0,
-  gender         text,
-  name           text,
-  dob            date,
+  updated int DEFAULT extract(epoch FROM NOW()),
+  eula int DEFAULT 0,
+  gender text,
+  name text,
+  dob date,
   activity_level smallint, -- [1, 2, 3, 4, 5]
-  goal_weight    real,
-  goal_bf        real,
-  bmr_eq_id      int,      -- ['HARRIS_BENEDICT', 'KATCH_MACARDLE', 'MIFFLIN_ST_JEOR', 'CUNNINGHAM']
-  bf_eq_id       int,      -- ['NAVY', '3SITE', '7SITE']
+  goal_weight real,
+  goal_bf real,
+  bmr_eq_id int, -- ['HARRIS_BENEDICT', 'KATCH_MACARDLE', 'MIFFLIN_ST_JEOR', 'CUNNINGHAM']
+  bf_eq_id int, -- ['NAVY', '3SITE', '7SITE']
   FOREIGN KEY (user_id) REFERENCES "user" (id),
   FOREIGN KEY (bf_eq_id) REFERENCES bf_eq (id),
   FOREIGN KEY (bmr_eq_id) REFERENCES bmr_eq (id)
 );
 
-CREATE TABLE recipe
-(
-  id      serial PRIMARY KEY,
-  guid    text NOT NULL UNIQUE,
-  user_id int  NOT NULL,
+CREATE TABLE recipe (
+  id serial PRIMARY KEY,
+  guid text NOT NULL UNIQUE,
+  user_id int NOT NULL,
   created int DEFAULT extract(epoch FROM NOW()),
   updated int DEFAULT extract(epoch FROM NOW()),
-  name    text NOT NULL,
+  name text NOT NULL,
   FOREIGN KEY (user_id) REFERENCES "user" (id)
 );
 
-CREATE TABLE recipe_dat
-(
-  recipe_id int  NOT NULL,
+CREATE TABLE recipe_dat (
+  recipe_id int NOT NULL,
   -- TODO: enforce FK constraint across two DBs?
-  food_id   int  NOT NULL,
-  grams     real NOT NULL,
-  notes     text,
+  food_id int NOT NULL,
+  grams real NOT NULL,
+  notes text,
   UNIQUE (recipe_id, food_id),
   FOREIGN KEY (recipe_id) REFERENCES recipe (id) ON UPDATE CASCADE
 );
 
-CREATE TABLE rda
-(
-  profile_id int  NOT NULL,
-  created    int DEFAULT extract(epoch FROM NOW()),
-  updated    int DEFAULT extract(epoch FROM NOW()),
+CREATE TABLE rda (
+  profile_id int NOT NULL,
+  created int DEFAULT extract(epoch FROM NOW()),
+  updated int DEFAULT extract(epoch FROM NOW()),
   -- TODO: move below SR, enforce FK constraint across nutr_def
-  nutr_id    int  NOT NULL,
-  rda        real NOT NULL,
+  nutr_id int NOT NULL,
+  rda real NOT NULL,
   UNIQUE (profile_id, nutr_id),
   FOREIGN KEY (profile_id) REFERENCES profile (id) ON UPDATE CASCADE
 );
@@ -201,13 +188,12 @@ CREATE TABLE rda
 --++++++++++++++++++++++++++++
 -- USDA SR Database
 --++++++++++++++++++++++++++++
-CREATE TABLE nutr_def
-(
-  id            int PRIMARY KEY,
-  rda           real,
-  units         text,
-  tagname       text NOT NULL,
-  nutr_desc     text NOT NULL,
+CREATE TABLE nutr_def (
+  id int PRIMARY KEY,
+  rda real,
+  units text,
+  tagname text NOT NULL,
+  nutr_desc text NOT NULL,
   anti_nutrient boolean,
   -- weighting?
   UNIQUE (tagname)
@@ -218,45 +204,41 @@ CREATE TABLE nutr_def
 ---------------------------
 -- TODO:  recommendation_foods (many-to-one: [food_ids...] --> rec_id)
 --     .. based on user upvotes/reporting?--
-CREATE TABLE rec_id
-(
-  id           serial PRIMARY KEY,
-  name         text,
+CREATE TABLE rec_id (
+  id serial PRIMARY KEY,
+  name text,
   serving_size text, -- NULL == "per rec.serving_size"
-  source_urls  text[] NOT NULL,
+  source_urls text[] NOT NULL,
   UNIQUE (name)
 );
 
-CREATE TABLE rec
-(
-  id           serial PRIMARY KEY,
-  rec_id       int  NOT NULL,
-  food_name    text NOT NULL,
-  serving_size text,   -- Display purposes only
-  notes        text,   -- e.g. visually segregate DAIRY vs. NON-DAIRY for calcium
-  source_urls  text[], -- If different from rec_id.source_urls
+CREATE TABLE rec (
+  id serial PRIMARY KEY,
+  rec_id int NOT NULL,
+  food_name text NOT NULL,
+  serving_size text, -- Display purposes only
+  notes text, -- e.g. visually segregate DAIRY vs. NON-DAIRY for calcium
+  source_urls text[], -- If different from rec_id.source_urls
   FOREIGN KEY (rec_id) REFERENCES rec_id (id) ON UPDATE CASCADE
 );
 
-CREATE TABLE rec_nut
-(
-  id         serial PRIMARY KEY,
-  rec_id     int NOT NULL,
-  nutr_id    int,
-  nutr_desc  text,
-  unit       text,
+CREATE TABLE rec_nut (
+  id serial PRIMARY KEY,
+  rec_id int NOT NULL,
+  nutr_id int,
+  nutr_desc text,
+  unit text,
   searchable boolean,
-  notes      text,
+  notes text,
   FOREIGN KEY (rec_id) REFERENCES rec (id) ON UPDATE CASCADE,
   FOREIGN KEY (nutr_id) REFERENCES nutr_def (id) ON UPDATE CASCADE
 );
 
-CREATE TABLE rec_dat
-(
-  id         int PRIMARY KEY,
-  entry_id   int  NOT NULL,
-  rec_nut_id int  NOT NULL,
-  nutr_val   text NOT NULL,
+CREATE TABLE rec_dat (
+  id int PRIMARY KEY,
+  entry_id int NOT NULL,
+  rec_nut_id int NOT NULL,
+  nutr_val text NOT NULL,
   -- nutr_val float NOT NULL,
   UNIQUE (entry_id, rec_nut_id),
   FOREIGN KEY (entry_id) REFERENCES rec (id) ON UPDATE CASCADE,
@@ -278,19 +260,17 @@ CREATE TABLE rec_dat
 --   FOREIGN KEY (user_id) REFERENCES "user" (id) ON UPDATE CASCADE
 -- );
 -- NOTE: wip
-CREATE TABLE bug
-(
-  id          bigserial PRIMARY KEY,
-  guid        uuid UNIQUE NOT NULL,
-  client_info jsonb       NOT NULL
+CREATE TABLE bug (
+  id bigserial PRIMARY KEY,
+  guid uuid UNIQUE NOT NULL,
+  client_info jsonb NOT NULL
 );
 
-CREATE TABLE msg
-(
-  id       serial PRIMARY KEY,
-  guid     uuid UNIQUE NOT NULL,
-  created  int,
+CREATE TABLE msg (
+  id serial PRIMARY KEY,
+  guid uuid UNIQUE NOT NULL,
+  created int,
   "header" text,
-  body     text
+  body text
 );
 
