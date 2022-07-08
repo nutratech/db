92779f48ebab4a007245aa6da6e72b2dd5b27811
Shane Jaroch <chown_tee@proton.me>
2022-07-08 10:08:11 -0400

    tidy, fix

--- sql/format.sh
+++ sql/format.sh
@@ -6,8 +6,8 @@ set -e
 cd "$(dirname "$0")"
 
 # Format tables & functions
-pg_format -s 2 tables.sql >tables.formatted.sql
+pg_format -L -s 2 tables.sql >tables.formatted.sql
 mv tables.formatted.sql tables.sql
 
-pg_format -s 2 functions.sql >functions.formatted.sql
+pg_format -L -s 2 functions.sql >functions.formatted.sql
 mv functions.formatted.sql functions.sql
