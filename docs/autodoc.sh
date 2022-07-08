6c8dbbfbbac36e658ba36c544e39ceb9323db6f5
Shane Jaroch <chown_tee@proton.me>
2022-07-08 10:23:58 -0400

    comment autodoc.sh, make work outside cwd

--- docs/autodoc.sh
+++ docs/autodoc.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 set -x
+
+cd "$(dirname "$0")"
 source .env
 
 set -e
 
-cd "$(dirname "$0")"
-
+# Define constants
 DB=nt
 SCHEMA=nt
 
+# Clean up previous run
 rm -rf $DB
 mkdir -p $DB
+
+# cd into specified directory
 cd $DB
 
 # Generate docs
@@ -23,8 +27,9 @@ postgresql_autodoc \
 	-f $SCHEMA \
 	-t dot
 
-# convert DOT --> EPS
-# convert EPS --> SVG
+# Generate PDF, HTML, etc
+#  convert DOT --> EPS
+#  convert EPS --> SVG
 dot -Tps $DB.dot -o $DB.eps
 epstopdf $DB.eps
 pdf2svg $DB.pdf $DB.svg
@@ -32,7 +37,7 @@ pdf2svg $DB.pdf $DB.svg
 # Move up
 mv $DB.svg ..
 
-# Clean up unless otherwise specified in .env file
+# Clean up (unless otherwise specified in .env file)
 if [ "0" != "$NTDB_AUTODOC_CLEANUP" ]; then
 	cd ..
 	rm -rf $DB
